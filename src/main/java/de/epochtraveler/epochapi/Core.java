f8fa55d511061e2ba3aa43696703865e1f6109bd
_JXc <hsvbuh12@gmail.com>
2023-04-09 11:39:50 +0200

    Updated to 1.5.2 - Added missing user object

--- src/main/java/de/epochtraveler/epochapi/Core.java
+++ src/main/java/de/epochtraveler/epochapi/Core.java
@@ -14,7 +14,7 @@ public final class Core {
     private static CoreServer coreServer;
 
     public static final String CORE_PREFIX = "§c§lAdministration §7| ";
-    public static final String CORE_VERSION = "1.5.1";
+    public static final String CORE_VERSION = "1.5.2";
 
     public Core(CoreServer pluginInstance)
     {
--- src/main/java/de/epochtraveler/epochapi/user/User.java
+++ src/main/java/de/epochtraveler/epochapi/user/User.java
@@ -1,5 +1,7 @@
 package de.epochtraveler.epochapi.user;
 
+import org.bukkit.entity.Player;
+
 public interface User
 {
     /**
@@ -14,9 +16,25 @@ public interface User
      */
     String getUsername();
 
+    /**
+     * Send a error message to the user
+     */
     void sendError();
 
+    /**
+     * Send a message to the user
+     */
     void sendMessage(String message);
+
+    /**
+     * Send a message with a custom prefix to the user
+     */
     void sendMessage(String prefix, String message);
+
+    /**
+     * Gets the player object
+     * @return Player
+     */
+    Player getPlayer();
 
 }
