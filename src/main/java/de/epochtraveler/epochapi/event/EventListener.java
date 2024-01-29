ea27b6a5094f2d72105bf90e9a4728a8c66bcf16
_JXc <hsvbuh12@gmail.com>
2024-01-29 21:56:06 +0100

    added javadoc

--- src/main/java/de/epochtraveler/epochapi/event/EventListener.java
+++ src/main/java/de/epochtraveler/epochapi/event/EventListener.java
@@ -2,12 +2,17 @@ package de.epochtraveler.epochapi.event;
 
 import org.bukkit.event.Event;
 
+/**
+ * A functional interface for handling events of a specific type.
+ *
+ * @param <T> The type of event this listener handles.
+ */
 public interface EventListener<T extends Event> {
 
     /**
-     * EventListener which gets executed when event is called
-     * @param event Event
+     * Handles the specified event.
+     *
+     * @param event The event to handle.
      */
     void handle(T event);
-
 }
