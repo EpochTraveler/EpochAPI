2d4fc6cf74ab782e7f161975e77c42b9413e560e
_JXc <hsvbuh12@gmail.com>
2024-01-29 21:57:44 +0100

    added javadoc

--- src/main/java/de/epochtraveler/epochapi/logging/Logger.java
+++ src/main/java/de/epochtraveler/epochapi/logging/Logger.java
@@ -1,41 +1,36 @@
 package de.epochtraveler.epochapi.logging;
 
+/**
+ * A simple logging interface for outputting messages at different log levels.
+ */
 public interface Logger {
 
     /**
-     * Logs a message with the info-level
-     * @param msg The logging message
+     * Logs a message with the info-level.
+     *
+     * @param msg The logging message.
      */
     void info(String msg);
 
     /**
-     * Logs a message with the warn-level
-     * @param msg The logging message
+     * Logs a message with the warn-level.
+     *
+     * @param msg The logging message.
      */
     void warn(String msg);
 
     /**
-     * Logs a message with the error-level
-     * @param msg The logging message
+     * Logs a message with the error-level.
+     *
+     * @param msg The logging message.
      */
     void error(String msg);
 
     /**
-     * Logs a message with the debug-level
-     * @param msg The logging message
+     * Logs a message with the debug-level.
+     *
+     * @param msg The logging message.
      */
     void debug(String msg);
-
-    /**
-     * Returns if the logger is enabled
-     * @return boolean
-     */
-    boolean isEnabled();
-
-    /**
-     * Toggle the logger
-     * @param enabled boolean
-     */
-    void setEnabled(boolean enabled);
 
 }
